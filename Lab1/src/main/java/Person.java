55350fe2705d338d7f5a35c166e692c3ab0df0a6
Nicole0509 <niashavril0509@gmail.com>
2025-10-04 22:36:29 +0200

    Created a person class and performed abstraction and it's members

---
+++ Lab1/src/main/java/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String names;
+    private String email;
+    private String phone;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
