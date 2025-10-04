8ea1e2820a5ed4f5f8aeed5c3708d9570652eb80
Nicole0509 <niashavril0509@gmail.com>
2025-10-04 23:08:46 +0200

    Rename Student to StudentDefinition and applyied abstraction on it

---
+++ Lab1/src/main/java/StudentPackage/StudentDefinition.java
@@ -0,0 +1,33 @@
+package StudentPackage;
+
+import SuperPackage.Person;
+
+import java.util.Date;
+
+
+public class StudentDefinition extends Person{
+    private Date dateOfBirth;
+    private String address;
+
+    public StudentDefinition(String names, String email, String phoneNumber, Date dateOfBirth, String address) {
+        super(names, email, phoneNumber);
+        this.setDateOfBirth(dateOfBirth);
+        this.setAddress(address);
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
--- Lab1/src/main/java/org/example/Main.java
+++ Lab1/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import StudentPackage.Student;
-import SuperPackage.Person;
+import StudentPackage.StudentDefinition;
+
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -9,10 +9,12 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         System.out.println("University Student Management System!");
-        Person person =new Student("Ada","ADA@gamil.com","+2507282938", Date.valueOf(LocalDate.now()),"Kimironko");
+        StudentDefinition person =new StudentDefinition("Ada","ADA@gamil.com","+2507282938", Date.valueOf(LocalDate.now()),"Kimironko");
 
         System.out.println(person.getNames());
         System.out.println(person.getEmail());
         System.out.println(person.getPhoneNumber());
+        System.out.println(person.getDateOfBirth());
+        System.out.println(person.getAddress());
     }
 }
