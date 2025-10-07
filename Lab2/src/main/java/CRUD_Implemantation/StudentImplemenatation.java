d89c63d970464e90c99f311036da1d8443c29a51
Nicole0509 <niashavril0509@gmail.com>
2025-10-07 12:10:16 +0200

    Successfully added a studnet to the table

--- Lab2/src/main/java/CRUD_Implemantation/StudentImplemenatation.java
+++ Lab2/src/main/java/CRUD_Implemantation/StudentImplemenatation.java
@@ -4,17 +4,39 @@ import CRUD_Interface.CrudInterface;
 import SuperPackage.CollectionManager;
 import models.Student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Date;
 
 public class StudentImplemenatation extends Student implements CrudInterface {
+    private final Connection connection;
+    String query;
 
-    public StudentImplemenatation(String names, String email, String phoneNumber, Date dateOfBirth, String address) {
+    public StudentImplemenatation(Connection connection, String names, String email, String phoneNumber, Date dateOfBirth, String address) {
         super(names, email, phoneNumber, dateOfBirth, address);
+        this.connection = connection;
     }
 
     @Override
     public void create(){
         System.out.println("Create a new student");
+        System.out.println(getNames());
+        System.out.println(getEmail());
+
+        query = "INSERT INTO students (names, email, phone_number, date_of_birth, address) VALUES (?, ?, ?, ?, ?)";
+
+        try(PreparedStatement statement = connection.prepareStatement(query)){
+
+            statement.setString(1, getNames());
+            statement.setString(2, getEmail());
+            statement.setString(3, getPhoneNumber());
+            statement.setDate(4, new java.sql.Date(getDateOfBirth().getTime()));
+            statement.setString(5, getAddress());
+            statement.executeUpdate();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
--- Lab2/src/main/java/org/example/Main.java
+++ Lab2/src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import CRUD_Implemantation.StudentImplemenatation;
+
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class Main {
 
@@ -10,12 +14,20 @@ public class Main {
     private static final String USERNAME = System.getenv("DB_USERNAME");
     private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
-    public static void main(String[] args) throws SQLException {
+    static void main(String[] args) throws SQLException {
 
         Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
         if (connection != null) {
-            System.out.println("Database connection established");
+//            Database connection established
+
+            StudentImplemenatation student =new StudentImplemenatation(connection,"Angela","angela@gamil.com","+2507282098", Date.valueOf(LocalDate.now()),"Kimironko");
+
+            student.create();
+
+
+            connection.close();
+
         } else  {
             System.out.println("Database connection could not be established");
         }
