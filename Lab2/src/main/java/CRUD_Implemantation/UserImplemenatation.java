2981532196ecc59c65cd90108d6bd3a93264d325
Nicole0509 <niashavril0509@gmail.com>
2025-10-07 11:11:19 +0200

    Cteated a Student implementation and overrode everthing in the Student class and the CRUD interface

---
+++ Lab2/src/main/java/CRUD_Implemantation/UserImplemenatation.java
@@ -0,0 +1,39 @@
+package CRUD_Implemantation;
+
+import CRUD_Interface.CrudInterface;
+import SuperPackage.CollectionManager;
+import models.Student;
+
+import java.util.Date;
+
+public class UserImplemenatation extends Student implements CrudInterface {
+
+    public UserImplemenatation(String names, String email, String phoneNumber, Date dateOfBirth, String address) {
+        super(names, email, phoneNumber, dateOfBirth, address);
+    }
+
+    @Override
+    public void create(){
+        System.out.println("Create a new student");
+    }
+
+    @Override
+    public void viewAll(){
+        System.out.println("View all students");
+    }
+
+    @Override
+    public void update(int i){
+        System.out.println("Update student");
+    }
+
+    @Override
+    public void delete(int i){
+        System.out.println("Delete student");
+    }
+
+    @Override
+    public boolean isFullTime(CollectionManager collectionManager) {
+        return false;
+    }
+}
--- Lab2/src/main/java/CRUD_Interface/CrudInterface.java
+++ Lab2/src/main/java/CRUD_Interface/CrudInterface.java
@@ -2,7 +2,7 @@ package CRUD_Interface;
 
 public interface CrudInterface {
     public void create();
-    public void read();
-    public void update();
-    public void delete();
+    public void viewAll();
+    public void update(int i);
+    public void delete(int i);
 }
