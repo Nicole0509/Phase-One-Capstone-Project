8de73bf7be1a01913596225d9534ee9a1c668b0a
Nicole0509 <niashavril0509@gmail.com>
2025-10-08 13:47:10 +0200

    Successfully connected to the DB via Lab3

--- Lab3/src/main/java/org/example/Main.java
+++ Lab3/src/main/java/org/example/Main.java
@@ -2,12 +2,29 @@ package org.example;
 
 import MenuPackage.MainMenu;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USERNAME = System.getenv("DB_USERNAME");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
-        MainMenu mainMenu = new MainMenu();
+    public static void main(String[] args) throws SQLException {
 
-        mainMenu.start();
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        if (connection != null) {
+            System.out.println("Database connection established");
+        } else  {
+            System.out.println("Database connection could not be established");
+        }
+
+
+//        MainMenu mainMenu = new MainMenu();
+//
+//        mainMenu.start();
     }
 }
