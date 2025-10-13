34ef0e5522350c18d9163976385259e37847d5e5
Nicole0509 <niashavril0509@gmail.com>
2025-10-13 20:43:59 +0200

    Managed to toggle through sections

---
+++ Lab3/src/main/webapp/index.js
@@ -0,0 +1,5 @@
+// Toggle Sections
+function showSection(sectionId){
+    document.querySelectorAll('.content-section').forEach(sec => sec.classList.add('hidden'));
+    document.getElementById(sectionId).classList.remove('hidden');
+}
